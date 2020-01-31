274fddb70f6c99ba9ba1ba4d72fe408d5d098f47
Anders Eknert <anders.eknert@bisnode.com>
2020-01-31 22:30:57 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='pre-commit-opa',
+    description='Pre-commit hooks for Open Policy Agent and Rego policies',
+    url='https://github.com/anderseknert/pre-commit-opa',
+    version_format='{tag}+{gitsha}',
+
+    author='Anders Eknert',
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    packages=find_packages(exclude=('tests*', 'testing*')),
+    install_requires=[
+        'setuptools-git-version',
+    ]
+    # entry_points={
+    #     'console_scripts': [
+    #         'terraform_docs_replace = pre_commit_hooks.terraform_docs_replace:main',
+    #     ],
+    # },
+)
