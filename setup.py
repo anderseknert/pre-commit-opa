e8922979af8b20e5d0978b4230a00a3dd850ed5d
Anders Eknert <anders.eknert@bisnode.com>
2020-02-01 15:43:56 +0100

    Add new files to setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,10 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
@@ -26,6 +28,9 @@ setup(
     ],
 
     scripts=[
+        'pre_commit_hooks/opa-check.py',
+        'pre_commit_hooks/opa-eval.py',
+        'pre_commit_hooks/opa-fmt.py',
         'pre_commit_hooks/opa-test.py',
     ]
 )
