83bff8124e8822cfb197865b3f4ccb00bf649875
Anders Eknert <anders.eknert@bisnode.com>
2020-02-07 15:59:13 +0100

    flake8 fixes

--- pre_commit_hooks/opa_check.py
+++ pre_commit_hooks/opa_check.py
@@ -4,12 +4,16 @@
 import sys
 import subprocess
 
+
 def main():
     """Runs 'opa check' on any files provided as arguments"""
-    process = subprocess.run(['opa', 'check'] + sys.argv[1:], capture_output=True)
+    process = subprocess.run(
+        ['opa', 'check'] + sys.argv[1:], capture_output=True
+    )
 
     if process.returncode > 0:
         sys.exit(process.stdout.decode('utf-8'))
+
 
 if __name__ == '__main__':
     main()
--- pre_commit_hooks/opa_fmt.py
+++ pre_commit_hooks/opa_fmt.py
@@ -4,12 +4,16 @@
 import sys
 import subprocess
 
+
 def main():
     """Runs 'opa fmt -w' on any files provided as arguments"""
-    process = subprocess.run(['opa', 'fmt', '-w'] + sys.argv[1:], capture_output=True)
+    process = subprocess.run(
+        ['opa', 'fmt', '-w'] + sys.argv[1:], capture_output=True
+    )
 
     if process.returncode > 0:
         sys.exit(process.stderr)
+
 
 if __name__ == '__main__':
     main()
