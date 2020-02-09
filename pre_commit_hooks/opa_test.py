212cfb4f836a52deda2f81263a88d9b9dcfa2e99
Anders Eknert <anders.eknert@bisnode.com>
2020-02-09 20:58:20 +0100

    First revision of test hook

--- pre_commit_hooks/opa_test.py
+++ pre_commit_hooks/opa_test.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
+"""OPA fmt pre-commit hook"""
 
-import os
 import sys
 import subprocess
 
-def main():
-    command = ['opa', 'test'] + sys.argv[1:]
-    process = subprocess.Popen(command)
 
-    if process:
-        if process.returncode > 0:
-            sys.exit(process.stderr)
-    else:
-        sys.exit('Failed to start OPA - please ensure it is installed and on $PATH')
+def main():
+    """Runs 'opa test' in git root directory or one provided in args"""
+    print(sys.argv)
+    process = subprocess.run(
+        ['opa', 'test'] + sys.argv[1:], capture_output=True
+    )
+
+    if process.returncode > 0:
+        sys.exit(process.stderr.decode('utf-8'))
+
 
 if __name__ == '__main__':
     main()
